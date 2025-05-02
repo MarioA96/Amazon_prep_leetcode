22f19fbc0b58f286f5c1cb4b2c29bd7cc514c0f4
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-05-01 20:01:06 -0600

    Added changes to challenge 33

--- 33_SearchinRotatedSortedArray.ts
+++ 33_SearchinRotatedSortedArray.ts
@@ -18,3 +18,13 @@ function search(nums: number[], target: number): number {
 
     return n;
 };
+
+function main(){
+    const nums = [4,5,6,7,0,1,2];
+    const target = 0;
+
+    const n = search(nums, target);
+    console.log("Searched number: ", n);
+}
+
+main();
