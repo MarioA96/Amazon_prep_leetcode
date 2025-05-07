5c11275c1dbf01b2dc49abba877a356680d80f74
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-05-06 20:14:34 -0600

    Add initial implementation of maxSubArray function with main execution

---
+++ theory/maxSubArray.ts
@@ -0,0 +1,12 @@
+function maxSubArray(nums: number[]): number {
+  
+    
+    return 0;
+};
+
+function main(){
+    const nums: number[] = [-2,1,3,4,-1,2,1,-5,4];
+    const output: number = maxSubArray(nums);
+};
+
+main();
