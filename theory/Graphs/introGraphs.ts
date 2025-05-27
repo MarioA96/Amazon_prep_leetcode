730a3c72ae0c209419c5fd514b2baf49c6bc40b9
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-05-26 21:18:19 -0600

    Implement Graph class with methods for adding vertices and edges, and displaying connections

--- theory/Graphs/introGraphs.ts
+++ theory/Graphs/introGraphs.ts
@@ -1,4 +1,35 @@
 
+class Graph {
+    public numberOfNode: number;
+    public adjacentList: {};
+
+    constructor(){
+        this.numberOfNode = 0;
+        this.adjacentList = {};
+    }
+
+    addVertex(node){
+
+    }
+
+    addEdge(node1, node2){
+        //undirected graph
+    }
+
+    showConnections(){
+        const allNodes = Object.keys;
+        (this.adjacentList);
+        for(let node of allNodes){
+            let nodeConnections = this.adjacentList[node];
+            let connections = "";
+            let vertex;
+            for(vertex of nodeConnections){
+                
+            }
+        }
+    }
+
+}
 
 function main(){
     
