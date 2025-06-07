af125be2c5c94a127d7a5a79db4c423132c694a9
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-06-06 23:48:18 -0600

    Implement validateTree function: add initial structure to validate if an array represents a binary search tree.

---
+++ algorithms/theory/searching/validateTree.ts
@@ -0,0 +1,19 @@
+// Given an array that represents a tree, validate if it is a BST, true if it is, false if not
+
+function validateTree(arr: number[]): boolean{
+
+    //CODE
+
+    return false;
+}
+
+function main(){
+
+    const treeArray_1 = [2,1,2];
+    const treeArray_2 = [5,1,4,null,null,3,6];
+
+    console.log(validateTree(treeArray_1));
+
+}
+
+main();
