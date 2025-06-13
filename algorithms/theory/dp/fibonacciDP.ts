5bec851c93533ed001e45ba6bc2f61b199fce858
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-06-12 22:43:18 -0600

    Refactor Fibonacci functions: separate calculations for recursive and memoized implementations

--- algorithms/theory/dp/fibonacciDP.ts
+++ algorithms/theory/dp/fibonacciDP.ts
@@ -1,9 +1,9 @@
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
 
-let calculations = 0;
+let calculations1 = 0;
 function fibonacci(n: number): number{
 
-    calculations++;
+    calculations1++;
     if(n<2){
         return n;
     }
@@ -11,10 +11,38 @@ function fibonacci(n: number): number{
 
 }
 
+
+let calculations2 = 0;
+let cache = {
+    0: 0,
+    1: 1,
+    2: 1
+};
+function fibonacciMemoized(n: number){
+
+    calculations2++;
+    if(n<2){
+        return cache[n];
+    }
+
+    if(n-2 in cache){
+        return cache[n-1] + cache[n-2];
+    } else {
+        cache[n-2] = fibonacciMemoized(n-2);
+        cache[n-1] = fibonacciMemoized(n-1);
+
+        return cache[n-1] + cache[n-2];
+    }
+
+}
+
 function main(){ 
 
     console.log("fibonacci: ", fibonacci(20));
-    console.log("calculations: ", calculations);
+    console.log("calculations: ", calculations1);
+
+    console.log("fibonacci: ", fibonacciMemoized(20));
+    console.log("calculations: ", calculations2);
 
 }
 
