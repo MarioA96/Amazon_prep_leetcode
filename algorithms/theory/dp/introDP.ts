ec2889f00a908d69785adb96eb6cfc5726790798
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-06-12 21:46:05 -0600

    Add closure-based memoization: implement memoizedAddTo80Closure function for improved caching

--- algorithms/theory/dp/introDP.ts
+++ algorithms/theory/dp/introDP.ts
@@ -17,10 +17,34 @@ function memoizedAddTo80(n: number){
     }
 }
 
+//Closure approach, help us avoiding a global variable
+// And so now we have more flexibility pointing the scope of the function
+function memoizedAddTo80Closure(){
+    let cache = {};
+
+    return function(n: number): number{
+        if(n in cache){
+            console.log("Inmediate cache result");
+
+            return cache[n];
+        }
+        else {
+            console.log("Takes a long time");
+            cache[n] = n + 80;
+
+            return cache[n];
+        }
+    }
+}
+
 function main(){
 
-    console.log( "1: ", memoizedAddTo80(5) );
-    console.log( "2: ", memoizedAddTo80(5) );
+    // console.log( "1: ", memoizedAddTo80(5) );
+    // console.log( "2: ", memoizedAddTo80(5) );
+
+    const memoized = memoizedAddTo80Closure();
+    console.log( "1: ", memoized(5) );
+    console.log( "2: ", memoized(5) );
 }
 
 main();
