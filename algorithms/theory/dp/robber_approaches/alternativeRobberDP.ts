42407dc1dd4fbcd65f97c4022f357b7ac8c96546
MarioA96 <marioaquirozs@gmail.com>
2025-07-14 22:22:25 -0600

    Moved files for solutions to "robber" problem to identify and keep track of future update

--- algorithms/theory/dp/robber_approaches/alternativeRobberDP.ts
+++ algorithms/theory/dp/robber_approaches/alternativeRobberDP.ts
@@ -1,4 +1,4 @@
-function rob(nums: number[]){
+function rob(nums: number[]): number{
 
     //Implementacion alternativa para la solucion del problema Robber
     const map: Map<number, number[]> = new Map<number, number[]>(); //valor: [posiciones]
@@ -28,14 +28,15 @@ function rob(nums: number[]){
 
                 while(condicion && j<set.length){
                     if(!set[j][0].has(sortedMap.get(keyOfMap).at(0)-1) && !set[j][0].has(sortedMap.get(keyOfMap).at(0)+1)){
-                        set[j][1] += keyOfMap;
                         set[j][0].add(sortedMap.get(keyOfMap).at(0));
+                        set[j][1] += keyOfMap;
                         condicion = false;
                     }
                     else if(!set[j+1]){ //No cumple la condicion de arriba y a parte no hay mas elementos
                         const subSetPosition = new Set<number>();
                         subSetPosition.add(sortedMap.get(keyOfMap).at(0));
                         set[j+1] = [subSetPosition, keyOfMap];
+                        condicion = false;
                     }
 
                     j++;
@@ -56,14 +57,15 @@ function rob(nums: number[]){
 
                     while(condicion && j<set.length){
                         if(!set[j][0].has(sortedMap.get(keyOfMap).at(i)-1) && !set[j][0].has(sortedMap.get(keyOfMap).at(i)+1)){
-                            set[j][1] += keyOfMap;
                             set[j][0].add(sortedMap.get(keyOfMap).at(i));
+                            set[j][1] += keyOfMap;
                             condicion = false;
                         }
                         else if(!set[j+1]){ //No cumple la condicion de arriba y a parte no hay mas elementos
                             const subSetPosition = new Set<number>();
                             subSetPosition.add(sortedMap.get(keyOfMap).at(i));
                             set[j+1] = [subSetPosition, keyOfMap];
+                            condicion = false;
                         }
 
                         j++;
@@ -73,37 +75,33 @@ function rob(nums: number[]){
 
         }
     }
-    // console.log(set.entries());
-    // set.set([sortedMap.get(10).at(0)], 10);
-    // set.set([sortedMap.get(10).at(1)], 10);
-    // set.set([sortedMap.get(10).at(2)], 10);
-    // const s: [[number[],number]] = [[[], 0]];
-    // s[0][1] = 10;
-    // s[0][0].push(sortedMap.get(10).at(0));
-    // s[0][0].push(sortedMap.get(10).at(1));
-    // s[0][0].push(sortedMap.get(10).at(2));
 
-    console.table(set);
+    const arraySols:number[] = [];
+    set.forEach((e)=>{
+        arraySols.push(e[1]);
+    });
+
+    return Math.max(...arraySols);
 }
 
 function main(){
 
-    const nums: number[] = [3,6,12,7,9,11,4];
+    // const nums: number[] = [3,6,12,7,9,11,4];
     // const nums: number[] = [6,3,10,8,2,10,3,5,10,5,3];
-    rob(nums);
-
+    // const nums: number[] = [1,2,3,1];
+    // const nums: number[] = [2,1,1,2];
+    // const nums: number[] = [2,7,9,3,1];
+    // const nums: number[] = [0,1,2,3];
+    // const nums: number[] = [3,6,12,7,9,11,4];
+    const nums:number[] = [
+        155,44,52,58,250,225,109,118,211,73,137,96,137,89,174,
+        66,134,26,25,205,239,85,146,73,55,6,122,196,128,50,61,
+        230,94,208,46,243,105,81,157,89,205,78,249,203,238,239,
+        217,212,241,242,157,79,133,66,36,165
+    ]
+    const solution: number = rob(nums);
+    console.log(solution)
 }
 
 main();
-
-
-// for(let i=0; i<nums.length; i++){
-//     if(map[nums[i]]){
-//         map[nums[i]] = [...map[nums[i]], i];
-//     } else {
-//         map[nums[i]] = [i];
-        
-//     }
-// }
-// console.log({...map});
     
