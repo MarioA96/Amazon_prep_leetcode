8b6b399ef2877ece04cdffa9aef0cc48a080f4f4
MarioA96 <marioaquirozs@gmail.com>
2025-07-17 22:26:33 -0600

    Improving algorithm

--- algorithms/theory/dp/robber_approaches/houseRobberRecursive.ts
+++ algorithms/theory/dp/robber_approaches/houseRobberRecursive.ts
@@ -1,39 +1,40 @@
 function houseRobber() {
     //[ak, ak-1, ..., a3, a2, a1, a0] -> [a0, a1, a2, ..., ak-1, ak]
-    const cache = {};
+    const cache: Map<number, number[]> = new Map<number, number[]>();
     
     return function rob(nums: number[]): number{
         let len = nums.length;
+
+        cache[0] = [nums[len-1]];
+        cache[1] = [[nums[len-1]],[nums[len-2]]];
+        cache[2] = [[nums[len-3]+nums[len-1]],[nums[len-2]]];
+        cache[3] = [[[nums[len-4]+Number(cache[1][0])], [nums[len-4]+Number(cache[1][1])]],[cache[2]]]; //[ [a2+[2,1]], [3,2] ]
+
         // 1: set.add(nums[len-1]),
         // 2: set.add(nums[len-1]).add(nums[len-2]),
         // 3: set.add(nums[len-3]+nums[len-1]).add(nums[len-2])
-        if(len in cache){
-            return cache[len];
-        }else {
-            if(len===1){ //[len, pos]->[K, K-1]->[1,0]
-                cache[0] = [nums[len-1]];
-                return cache[0];
-            }
-            else if(len===2){ //[K-1, K-2]->[2,1]
-                cache[1] = [[nums[len-1]],[nums[len-2]]];
-                return cache[1];
-            }
-            else if(len===3){ //[K-2, K-3]->[3,2]
-                cache[2] = [[nums[len-3]+nums[len-1]],[nums[len-2]]];
-                return cache[2];
-            }
-            else if(len===4){ //[K-3, K-4]->[4,3]
-                cache[3] = [[nums[len-4]+cache[2]],[cache[3]]]; //[ [a2+[2,1]], [3,2] ]
-                return cache[3];
-            }
-            else {
-                cache[len-1] = [
-                    [nums[len-1] + rob(nums.slice(2, len-1))], //[ak]+[K-2, k-3]
-                    [rob(nums.slice(1, len-1))]
-                ];
-                return cache[len-1];
-            }
+        if(len===1){ //[len, pos]->[K, K-1]->[1,0]
+            return cache[0];
         }
+        else if(len===2){ //[K-1, K-2]->[2,1]
+            return cache[1];
+        }
+        else if(len===3){ //[K-2, K-3]->[3,2]
+            return cache[2];
+        }
+        else if(len===4){ //[K-3, K-4]->[4,3]
+            return cache[3];
+        }
+        else {
+            console.log(nums.slice(2, len));
+            console.log(nums.slice(1, len));
+            cache[len-1] = [
+                [ [nums[0] + Number(rob(nums.slice(2, len))[0]) ], [nums[0] + Number(rob(nums.slice(2, len))[1]) ] ], //[ak]+[K-2, k-3]
+                [ rob(nums.slice(1, len)) ]
+            ];
+            return cache[len-1];
+        }
+        
     }
 
 }
@@ -42,8 +43,8 @@ function main(){
     // const nums: number[] = [3,6,12,7,9,11,4];
     // const nums: number[] = [6,3,10,8,2,10,3,5,10,5,3];
     // const nums: number[] = [1,2,3,1];
-    const nums: number[] = [2,1,1,2];
-    // const nums: number[] = [2,7,9,3,1];
+    // const nums: number[] = [2,1,1,2];
+    const nums: number[] = [2,7,9,3,1];
     // const nums: number[] = [0,1,2,3];
     // const nums: number[] = [3,6,12,7,9,11,4];
     // const nums:number[] = [
