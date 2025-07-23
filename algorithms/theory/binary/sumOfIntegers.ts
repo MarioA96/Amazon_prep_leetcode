d3771cd0bc1d1033ef18e47b5fb214017d784385
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-07-22 22:44:27 -0600

    First Implement bitwise sum calculation for two integers

--- algorithms/theory/binary/sumOfIntegers.ts
+++ algorithms/theory/binary/sumOfIntegers.ts
@@ -4,4 +4,22 @@ Given two integers a and b, return the sum of the two integers without using the
 
 function getSum(a: number, b: number): number {
     
+    let generate:number;
+    let propagate: number;
+
+    generate = a & b;
+    propagate = a | b;
+
+    console.log("Generate: ", generate.toString(2), ", Propagate: ", propagate.toString(2));
+
+    return 0;
 };
+
+function main(){
+    const number_1: number = 10; //0001010
+    const number_2: number = 11; //0001011   
+
+    console.log("Exec output: ", getSum(number_1, number_2));
+}
+
+main();
