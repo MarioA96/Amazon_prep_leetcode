5a5cda008385abe758314604c3a43bdfac06fb60
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-07-24 22:48:13 -0600

    Added exmaples.

--- algorithms/theory/fast_slow_pointers/addTwoNumbers.ts
+++ algorithms/theory/fast_slow_pointers/addTwoNumbers.ts
@@ -3,6 +3,15 @@ You are given two non-empty linked lists representing two non-negative integers.
 
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 */
+/*
+Examples:
+    Input: l1 = [2,4,3], l2 = [5,6,4]
+    Output: [7,0,8]
+    Explanation: 342 + 465 = 807.
+
+    Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+    Output: [8,9,9,9,0,0,0,1]
+*/
 // Definition for singly-linked list.
 class ListNode {
     val: number
