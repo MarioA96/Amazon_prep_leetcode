62d84c452bd244266dbcbe69ad835ca449818544
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-07-31 21:18:41 -0600

    Final result algorithm for the problem.

--- algorithms/theory/binary/numberOfOneBits.ts
+++ algorithms/theory/binary/numberOfOneBits.ts
@@ -1,12 +1,20 @@
 function hammingWeight(n: number): number {
     
+    let counter = 0;
 
-    return 0;
+    while(n !== 0){
+        if ((n & 1) === 1) {
+            counter++;
+        }
+        n = n >> 1;
+    }
+
+    return counter;
 };
 
 function main(){
 
-    const input: number = 17;
+    const input: number = 2147483645;
     console.log(hammingWeight(input));
 
 }
