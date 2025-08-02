b67f4c38599b01c6a7da1fdcfae8c47d5450269a
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-08-01 21:38:20 -0600

    Second attempt for solving the problem.

--- algorithms/theory/arrays/maxAreaWaterContainer.ts
+++ algorithms/theory/arrays/maxAreaWaterContainer.ts
@@ -7,59 +7,89 @@ function maxArea(height: number[]): number {
     let posI0 = 0;
     let posIF = len-1;
 
-    let i0:number = height[posI0]!;
-    let iF:number = height[posIF]!;
+    let i0 = 0;
+    let iF = 0;
 
     let condI0 = false;
     let condIF = false;
     //Calculamos el area bXh
-    let b = posIF - posI0;
+    let b = 0;
     let h = 0;
 
-    if(i0<iF){
-        h = i0;
-        condI0 = true;
-        condIF = false;
-    } else if(i0>iF){
-        h = iF;
-        condI0 = false;
-        condIF = true;
-    } else {
-        //Da igual pues tienen el mismo tamano
-        h = i0;
-        condI0 = false;
-        condIF = false;
+    let areMoreMaxArea = true;
+
+    while( (posI0 !== posIF) && areMoreMaxArea){
+        i0 = height[posI0]!;
+        iF = height[posIF]!;
+        b = posIF - posI0;
+
+        // console.log("b: ", b, ", posI0: ", posI0, ", posIF: ", posIF);
+
+        if(i0<iF){
+            h = i0;
+            condI0 = true; //Si puntero inicial es menor que puntero final
+            condIF = false;
+        } else if(i0>iF){
+            h = iF;
+            condI0 = false;
+            condIF = true;  //Si puntero final es menor que puntero inicial
+        } else {
+            //Da igual pues tienen el mismo tamano
+            h = i0;
+            condI0 = false;
+            condIF = false;
+        }
+
+        area = b*h;
+
+        if(maxArea === 0){
+            maxArea = area; 
+        } else if(maxArea < area){
+            maxArea = area;
+        } else if(maxArea >= area){
+            maxArea;
+        }
+
+        if(condI0){ // Si el el valor del puntero1 es menor que valor del puntero2
+            // Muevo el puntero1
+            posI0++;
+        } else if(condIF){
+            // Sino muevo el puntero2
+            posIF--;
+        } else {
+            // Sino debo buscar el numero mas grande de la izq o derecha
+            let condBusqueda = true;
+            let tempBase = 0;
+            let noMoreElements = false;
+            while(condBusqueda && !noMoreElements){
+                if(height[posI0]! > height[posIF]!){
+                    posI0++;
+                    condBusqueda = false;
+                } else if(height[posI0]! < height[posIF]!){
+                    posIF--;
+                    condBusqueda = false;
+                } else if(height[posI0]! === height[posIF]!){
+                    posI0++;
+                    posIF--;
+                    tempBase = posIF - posI0;
+                    if(tempBase <= 0){
+                        noMoreElements = true;
+                        areMoreMaxArea = false;
+                    }
+                }
+            }
+        }
     }
 
-    area = b*h;
-
-    if(maxArea === 0){
-        maxArea = area; 
-    } else if(maxArea < area){
-        maxArea = area;
-    } else if(maxArea >= area){
-        maxArea;
-    }
-
-    if(condI0){ // Si el el valor del puntero1 es menor que valor del puntero2
-        // Muevo el puntero1
-        posI0++;
-    } else if(condIF){
-        // Sino muevo el puntero2
-        posIF--;
-    } else {
-        // Sino debo buscar el numero mas grande de la izq o derecha
-    }
-
-    //FALTA CICLAR TODO EL PROCESO DE CONDICIONES HASTA QUE NO HAY MAS ELEMENTOS  Y HACER LA BUSQUEDA
-
-    console.log(maxArea);
-    return 0;
+    
+    return maxArea;
 };
 
 function main(){
 
-    const input:number[] = [1,8,6,2,5,4,8,3,7];
+    // const input:number[] = [1,8,6,2,5,4,8,3,7];
+    // const input:number[] = [1,1,1,1,1];
+    const input:number[] = [1,1]
     const output:number = maxArea(input);
 
     console.log(output);
