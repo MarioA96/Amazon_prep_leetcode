89896c99995d41c7e30387ed06029f33ebe7fd31
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-08-04 23:19:54 -0600

    Added initial implementation of problem.

---
+++ algorithms/theory/dp/longestIncreasingSubsequence.ts
@@ -0,0 +1,19 @@
+/*
+Given an integer array nums, return the length of the longest strictly increasing subsequences.
+*/
+
+function lengthOfLIS(nums: number[]): number {
+    
+
+};
+
+function main(){
+
+    const nums = [10,9,2,5,3,7,101,18];
+    const result = lengthOfLIS(nums);
+
+    console.log(result);
+
+}
+
+main();
