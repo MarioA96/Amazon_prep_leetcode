8f2b774cf96e5593cd596cf457b11286d592dc83
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-08-04 22:30:01 -0600

    Fixed logic state of problem, aditionally added a new strategy via BottomUp DP.

--- algorithms/theory/dp/climbingStairs.ts
+++ algorithms/theory/dp/climbingStairs.ts
@@ -9,7 +9,7 @@ function climbStairsRecursive(n: number): number {
         return n;
     }
     else{
-        return climbStairsRecursive(n-1) + 2;
+        return climbStairsRecursive(n-1) + climbStairsRecursive(n-2);
     }
 
 };
@@ -25,7 +25,7 @@ function climbStairsMemoized() {
             if(n<=3){
                 return n;
             } else {
-                state[n] = closure(n-1)+2;
+                state[n] = closure(n-1)+closure(n-2);
                 return state[n];
             }
         }
@@ -33,15 +33,30 @@ function climbStairsMemoized() {
 
 }
 
+function climbStairsBottomUp(n:number): number{
+
+    let current = [1,2,3];
+    if(n<=3){
+        return n;
+    }
+    for(let i=3; i<n; i++){
+        current.push( current[i-1]!+current[i-2]! );
+    }
+
+    return current.pop()!;
+}
+
 function main(){
 
-    const numberStairs = 45;
+    const numberStairs = 40;
     // console.log(result);
 
     // const result = climbStairsRecursive(numberStairs);
-    const memo = climbStairsMemoized();
-    console.log(memo(numberStairs));
+    // const memo = climbStairsMemoized();
+    // console.log(memo(numberStairs));
 
+    const result = climbStairsBottomUp(numberStairs);
+    console.log(result);
 }
 
 main();
