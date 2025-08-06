5c3de8c0a1eaace3f29f687d3251dbfa80c7a8f9
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-08-05 21:59:45 -0600

    Division attempt for solving DP problem.

--- algorithms/theory/dp/longestIncreasingSubsequence.ts
+++ algorithms/theory/dp/longestIncreasingSubsequence.ts
@@ -2,10 +2,38 @@
 Given an integer array nums, return the length of the longest strictly increasing subsequences.
 */
 
-function lengthOfLIS(nums: number[]): number {
+function lengthOfLIS(nums: number[]): number[] {
     
+    let len = nums.length;
+
+    if(len===1){
+        return nums;
+    }
+    if(len===2){
+        if(nums[0]!<nums[1]!){ // Envio ambos elementos   
+            return nums;
+        }
+        else { //Envio el arreglo con el elemento mas pequeno
+            const el = nums.pop();
+            nums[0] = el!;
+            return nums;
+        }
+    }
+    const left = nums.slice(0, Math.ceil(len/2));
+    const right = nums.slice(Math.ceil(len/2), len);
+
+    return lengthSub(
+        lengthOfLIS(left),
+        lengthOfLIS(right)
+    );
 
 };
+
+function lengthSub(arr1: number[], arr2: number[]): number[]{
+
+    
+
+}
 
 function main(){
 
