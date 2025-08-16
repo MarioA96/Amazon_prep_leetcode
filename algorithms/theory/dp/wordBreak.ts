4df3e068a4b1b825221a36193f3bf725ffd1144f
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-08-15 23:12:11 -0600

    Added more cases fpr DP.

--- algorithms/theory/dp/wordBreak.ts
+++ algorithms/theory/dp/wordBreak.ts
@@ -7,18 +7,31 @@ Note that the same word in the dictionary may be reused multiple times in the se
 
 function wordBreak(s: string, wordDict: string[]): boolean {
     
-    if(wordDict.length > 1){
-        let wordDictLeft = wordDict.slice(0, Math.ceil(wordDict.length / 2));
-        let wordDictRight = wordDict.slice(Math.ceil(wordDict.length / 2), wordDict.length - 1);
-
-        console.log("left: ", wordDictLeft, ", right: ", wordDictRight);
+    if(wordDict.length < 1){
+        if(s.length===0){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
     if(wordDict.length === 1){
         let stringWordDict = wordDict.toString();
         console.log(stringWordDict);
+
+        return true;
+    }
+    if(wordDict.length > 1){
+        let wordDictLeft = wordDict.slice(0, Math.ceil(wordDict.length / 2));
+        let wordDictRight = wordDict.slice(Math.ceil(wordDict.length / 2), wordDict.length);
+
+        console.log("left: ", wordDictLeft, ", right: ", wordDictRight);
+        let res = [
+            wordBreak(s, wordDictLeft),
+            wordBreak(s, wordDictRight)
+        ];
     }
 
-    
 
     return false;
 };
