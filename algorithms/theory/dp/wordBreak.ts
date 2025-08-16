57bb331a7a9ee00c1ab1077f6afc6597404dcd18
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-08-15 22:31:41 -0600

    Added cases

--- algorithms/theory/dp/wordBreak.ts
+++ algorithms/theory/dp/wordBreak.ts
@@ -7,6 +7,18 @@ Note that the same word in the dictionary may be reused multiple times in the se
 
 function wordBreak(s: string, wordDict: string[]): boolean {
     
+    if(wordDict.length > 1){
+        let wordDictLeft = wordDict.slice(0, Math.ceil(wordDict.length / 2));
+        let wordDictRight = wordDict.slice(Math.ceil(wordDict.length / 2), wordDict.length - 1);
+
+        console.log("left: ", wordDictLeft, ", right: ", wordDictRight);
+    }
+    if(wordDict.length === 1){
+        let stringWordDict = wordDict.toString();
+        console.log(stringWordDict);
+    }
+
+    
 
     return false;
 };
