dd11aca6c750915e65becf9ead1e01afd717bc56
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-08-15 23:19:13 -0600

    Restricteed cases of analysis

--- algorithms/theory/dp/wordBreak.ts
+++ algorithms/theory/dp/wordBreak.ts
@@ -7,6 +7,8 @@ Note that the same word in the dictionary may be reused multiple times in the se
 
 function wordBreak(s: string, wordDict: string[]): boolean {
     
+    let minimum = Infinity;
+
     if(wordDict.length < 1){
         if(s.length===0){
             return true;
@@ -16,20 +18,24 @@ function wordBreak(s: string, wordDict: string[]): boolean {
         }
     }
     if(wordDict.length === 1){
-        let stringWordDict = wordDict.toString();
-        console.log(stringWordDict);
-
-        return true;
+        let stringResult = s.replace(wordDict[0]!, ""); 
+        if(stringResult.length===0){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
     if(wordDict.length > 1){
-        let wordDictLeft = wordDict.slice(0, Math.ceil(wordDict.length / 2));
-        let wordDictRight = wordDict.slice(Math.ceil(wordDict.length / 2), wordDict.length);
+        // let wordDictLeft = wordDict.slice(0, Math.ceil(wordDict.length / 2));
+        // let wordDictRight = wordDict.slice(Math.ceil(wordDict.length / 2), wordDict.length);
 
-        console.log("left: ", wordDictLeft, ", right: ", wordDictRight);
-        let res = [
-            wordBreak(s, wordDictLeft),
-            wordBreak(s, wordDictRight)
-        ];
+        // console.log("left: ", wordDictLeft, ", right: ", wordDictRight);
+        // let res = [
+        //     wordBreak(s, wordDictLeft),
+        //     wordBreak(s, wordDictRight)
+        // ];
+        
     }
 
 
