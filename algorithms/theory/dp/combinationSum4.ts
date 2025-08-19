5bf26f14bd8fa541cb27d3085f6b0dc566e0af3b
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-08-19 01:53:44 -0600

    Initial stablish of problem and initial functions.

---
+++ algorithms/theory/dp/combinationSum4.ts
@@ -0,0 +1,20 @@
+/*
+Given an array of distinct integers nums and a target integer target, return the number of 
+possible combinations that add up to target.
+
+The test cases are generated so that the answer can fit in a 32-bit integer.
+*/
+
+function combinationSum4(nums: number[], target: number): number{
+
+
+    return 0;
+}
+
+function main(){
+
+
+
+}
+
+main();
