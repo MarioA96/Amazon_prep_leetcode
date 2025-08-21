945bd60e2a42daa0b7f8cc75895ed7e21255ca0a
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-08-20 19:40:47 -0600

    Refactor combinationSum4 function to streamline logic and improve memoization

--- algorithms/theory/dp/combinationSum4.ts
+++ algorithms/theory/dp/combinationSum4.ts
@@ -7,50 +7,37 @@ The test cases are generated so that the answer can fit in a 32-bit integer.
 
 function combinationSum4(nums: number[], target: number): number{
 
-    let i: number = 0;
     const memo: Map<number, number> = new Map<number, number>()
 
-    if(target===0){
-        return 1;
-    }
-    if(target<0){
-        return 0;
-    }
-    if(target>0){
-        DP(nums, target, memo, i);
-    }
-
-    return memo.size;
-}
-
-function DP(nums: number[], target: number, memo: Map<number,number>, i: number): number{
-
-    if(memo.has(i)){
-        return memo.get(i)!;
-    }
-    
-    for(i=0; i<nums.length; i++){
-        target = target - nums[i]!;
-
-        if(target<0){
-            return 0;
-        }
-        if(target===0){
-            memo.set(i,1);
+    function DP(t: number): number{
+        if(t===0){
             return 1;
         }
-        if( target>0 ){
-            DP(nums, target, memo, i);
+        if(t<0){
+            return 0;
         }
+        if(memo.has(t)){
+            return memo.get(t)!;
+        }
+
+        let total = 0;
+        for (const num of nums) {
+            total += DP(t - num);
+        }
+
+        memo.set(t, total);
+        return total;
     }
 
-    return 0;
+    return DP(target); //Aplicando Closure y mas apegado a un algoritmo DP funcional
 }
+
+
 
 function main(){
 
     const nums = [1,2,3];
-    const target = 7;
+    const target = 4;
 
     const output = combinationSum4(nums, target);
 
