5bcfb133b7bc565f4730a5b1e17c3f8ad60c3df8
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-08-27 21:58:34 -0600

    Refactor houseRobber2 functions to improve clarity and consistency in naming

--- algorithms/theory/dp/houseRobber2.ts
+++ algorithms/theory/dp/houseRobber2.ts
@@ -2,8 +2,48 @@
 Given an integer array nums representing the amount of money of each house, 
 return the maximum amount of money you can rob tonight without alerting the police.
 */
+function robMemoization(nums: number[]): number{
 
-function rob(nums: number[]): number {
+    //Caso trivial
+    if(nums.length===1){
+        return nums[0]!;
+    }
+
+    //Caso circular, excluimos primer o ultimo elemento
+    return Math.max(
+        robLinear(nums, 0, nums.length-2), // Robamos casas desde 0 a n-2
+        robLinear(nums, 1, nums.length-1)  // Robamos casas desde 1 a n-1
+    );
+
+    function robLinear(nums: number[], start: number, end: number): number{
+        const memo: Map<number, number> = new Map<number,number>();
+
+        function dp(i: number): number{
+            //Caso base, si pasamos antes de start, no hay nada que robar
+            if(i<start){
+                return 0;
+            }
+            if(memo.has(i)){
+                return memo.get(i)!;
+            }
+
+            //Opcion 1: Tomar casa i y saltar a i-2
+            let take = nums[i]! + dp(i-2);
+            //Opcion 2: No tomar ca i e ir a i-1
+            let no_take = dp(i-1);
+
+            let result = Math.max(take,no_take); // max(nums[i]+f(i+2), f(i+1))
+            
+            memo.set(i, result);
+            return result;
+        }
+
+        return dp(end);
+    }
+}
+
+
+function robTabulation(nums: number[]): number {
     
     const len = nums.length;
     
@@ -11,10 +51,10 @@ function rob(nums: number[]): number {
         return nums[0]!;
     }
 
-    const case1 = dp(nums, 0, len-2);
-    const case2 = dp(nums, 1, len-1);
+    const case1 = robLinear(nums, 0, len-2);
+    const case2 = robLinear(nums, 1, len-1);
 
-    function dp(nums: number[], start: number, end: number): number{
+    function robLinear(nums: number[], start: number, end: number): number{
 
         let prevRes_i_2 = 0;
         let prevRes_i_1 = 0;
@@ -36,10 +76,13 @@ function rob(nums: number[]): number {
 
 function main(){
 
-    const nums:number[] = [2,3,2];
-    const output: number = rob(nums);
+    const nums:number[] = [1,2,3,1];
 
-    console.log(output);
+    let output: number = robMemoization(nums);
+    console.log("Memoization result: ", output);
+
+    output = robTabulation(nums);
+    console.log("Tabulation result: ", output);
 
 }
 
