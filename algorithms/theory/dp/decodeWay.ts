b04331bafd14a05a105f4bb08aae0ca4ec434890
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-08-28 21:20:14 -0600

    Add initial implementation of numDecodings function to calculate decoding ways

---
+++ algorithms/theory/dp/decodeWay.ts
@@ -0,0 +1,21 @@
+/*
+Given a string s containing only digits, return the number of ways to decode it. 
+If the entire string cannot be decoded in any valid way, return 0.
+
+The test cases are generated so that the answer fits in a 32-bit integer.
+*/
+
+function numDecodings(s: string): number {
+    
+
+    return 0;
+};
+
+function main(){
+    const word: string = "12";
+    const output: number = numDecodings(word);
+
+    console.log(output);
+};
+
+main();
