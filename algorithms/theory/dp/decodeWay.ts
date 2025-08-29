af7b41f1a0a06116ae0497a535868175e70bedb6
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-08-28 22:51:35 -0600

    Fix numDecodings function to handle single and double digit cases correctly

--- algorithms/theory/dp/decodeWay.ts
+++ algorithms/theory/dp/decodeWay.ts
@@ -7,12 +7,29 @@ The test cases are generated so that the answer fits in a 32-bit integer.
 
 function numDecodings(s: string): number {
     
+    if(s.length===1){
+        if(s[0]! ==='0') return 0;
+        else return 1;
+    }
+    if(s.length===2){
+        if(s[0] === '0') return 0;
+        if(Number(s[0]) > 2) return 1;
+        if(s[0] === '1'){
+            if(s[1] === '0') return 1;
+            if(Number(s[1]) > 0) return 2;
+        }
+        if(s[0] === '2'){
+            if(s[1] === '0') return 1;
+            if(Number(s[1])>=1 && Number(s[1])<=6) return 2;
+            if(Number(s[1])>6) return 1;
+        }
+    }
 
     return 0;
 };
 
 function main(){
-    const word: string = "12";
+    const word: string = "10";
     const output: number = numDecodings(word);
 
     console.log(output);
