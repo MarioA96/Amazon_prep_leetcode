02b20ca6927b0c3b37ec770700809531649cd844
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-09-05 20:53:25 -0600

    Add tabulation-based implementation of numDecodings function for improved performance

--- algorithms/theory/dp/decodeWay.ts
+++ algorithms/theory/dp/decodeWay.ts
@@ -37,9 +37,35 @@ function numDecodings(s: string): number {
     return dp(0);
 };
 
+function numDecodings_tab(s: string): number{
+
+    const n = s.length;
+    let dp = new Array<number>(n+1);
+    dp.fill(0);
+
+    // Caso base: string vacío se decodifica de 1 manera (trivial)
+    dp[n] = 1;
+
+    // Recorremos desde el final hacia el inicio
+    for(let i=n-1; i>=0; i--){
+        if(s[i] !== '0'){
+            // Tomar 1 digito
+            dp[i]! += dp[i+1]!;
+
+            // Tomamos 2 digitos
+            if( ((i+1)<n) && (s[i]==='1' || (s[i]==='2'&&s[i+1]!<='6'))  ){
+                dp[i]! += dp[i+2]!;
+            }
+        }
+    }
+
+    return dp[0]!
+}
+
 function main(){
     const word: string = "2611055971756562";
-    const output: number = numDecodings(word); //4 no 2
+    let output: number = numDecodings(word); //4 no 2
+    output = numDecodings_tab(word);
 
     console.log(output);
 };
